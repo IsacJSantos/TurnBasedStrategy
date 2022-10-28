857b8a83c0179e894a8384a3cd16abf24c8785dd
MSI\Isac <isac43222gmail.com>
2022-10-27 21:18:42 -0300

    Unit Rotate when moving

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField]
+    float rotateSpeed = 10f;
+
+    [SerializeField]
     private Animator unitAnimator;
     private Vector3 targetPosition;
 
@@ -13,16 +16,20 @@ public class Unit : MonoBehaviour
             Move(MouseWorld.GetPosition());
         }
 
-       
+
         float stoppingDistance = .1f;
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4.0f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
             unitAnimator.SetBool("IsWalking", true);
         }
-        else 
+        else
         {
             unitAnimator.SetBool("IsWalking", false);
         }
