227bc817509f8f1afdff72ca9e31fb7ac94e2243
MSI\Isac <isac43222gmail.com>
2022-11-02 21:21:19 -0300

    Grid Object

---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,12 @@
+
+
+public class GridObject  
+{
+    private GridSystem gridSystem;
+    private GridPosition gridPosition;
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        this.gridSystem = gridSystem;
+        this.gridPosition = gridPosition;
+    }
+}
--- Assets/Scripts/GridSystem.cs
+++ Assets/Scripts/GridSystem.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GridSystem
 {
-    int width;
-    int height;
-    float cellSize;
+    private int width;
+    private int height;
+    private float cellSize;
+    private GridObject[,] gridObjectArray;
 
     public GridSystem(int width, int height, float cellSize)
     {
@@ -14,11 +13,14 @@ public class GridSystem
         this.height = height;
         this.cellSize = cellSize;
 
+        gridObjectArray = new GridObject[width, height];
+
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
-                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * .2f, Color.white, 1000);
+                GridPosition gridPosition = new GridPosition(x, z);
+                gridObjectArray[x, z] = new GridObject(this, gridPosition);
             }
         }
     }
@@ -34,5 +36,16 @@ public class GridSystem
              Mathf.RoundToInt(worldPosition.x / cellSize),
              Mathf.RoundToInt(worldPosition.z / cellSize)
             );
+    }
+
+    public void CreateDebugObjects(Transform debugPrefab) 
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GameObject.Instantiate(debugPrefab,GetWorldPosition(x,z),Quaternion.identity);
+            }
+        }
     }
 }
